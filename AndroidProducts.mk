1db4db2a8c0c02d2586b473cfd93b8a2349be954
unmoved21 <unmoved2121@gmail.com>
2023-03-09 21:06:33 +0100

    Calyxos build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sweet.mk
+    $(LOCAL_DIR)/calyx_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_sweet-user \
-    aosp_sweet-userdebug \
-    aosp_sweet-eng
+    calyx_sweet-user \
+    calyx_sweet-userdebug \
+    calyx_sweet-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+# clone
+git clone -b 16 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
+git clone -b aghisna-13 --depth=1 https://github.com/RooGhz720/Aghisna_Sweet_Kernel.git kernel/xiaomi/sm6150
